54caaa4e9478531f8309aded95441422d4d1a2aa
Drakezk <khoi30925@gmail.com>
2024-12-03 20:15:26 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     echo $_POST['agreee'];
 }
 ?>
+    
 <form  method="post">
     <input type="checkbox" name="agreee" value="2">
     <button type="submit">ok</button>
